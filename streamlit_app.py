d2a493201ffaa10c255969ecf2212aa9d48c9915
engr-alva <anna.lorraine.aquino@accenture.com>
2024-10-29 01:04:13 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,32 +4,37 @@ from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title("Customize your Smoothie :cup_with_straw:")
-st.write(
-    """Replace this example with your own code!
-    **And if you're new to Streamlit,** check
-    out our easy-to-follow guides at
-    [docs.streamlit.io](https://docs.streamlit.io).
-    """
-)
+st.write("""Orders that need to be filled.""")
 
+import streamlit as st
 
 name_on_order = st.text_input('Name on Smoothie:')
-st.write('The name on your Smoothie will be: ', name_on_order)
+st.write("The name on your Smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
-ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe)
+
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
+
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients",
+    my_dataframe,
+)
 
 if ingredients_list:
-
-    ingredients_string = ''
-
+    ingredients_string =''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+    
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
@@ -38,9 +43,3 @@ if ingredients_list:
     st.write(my_insert_stmt)
     st.stop()
     
-    time_to_insert = st.button('Submit Order')
-    
-    if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        
-        st.success('Your Smoothie is ordered!', icon="✅")
